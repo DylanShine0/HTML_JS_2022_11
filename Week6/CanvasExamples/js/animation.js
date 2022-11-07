303383e143b8f3881f9cd4c2e7468367ed853a85
Dylan Shine <djshine@email.neit.edu>
2022-11-07 12:24:52 -0500

    edits

--- Week6/CanvasExamples/js/animation.js
+++ Week6/CanvasExamples/js/animation.js
@@ -1,7 +1,23 @@
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
 
-ctx.fillStyle = "red";
-ctx.beginPath();
-ctx.arc(400, 300, 20, 0, 2*Math.PI, true)
-ctx.fill()
+var timer = requestAnimationFrame(main);
+var x = 100;
+
+
+function main() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(x, 300, 20, 0, 2 * Math.PI, true)
+    ctx.fill()
+    x += 5;
+    
+    if(x > 800 + 20){
+        x = -20;
+    }
+
+    timer = requestAnimationFrame(main)
+}
+
+
